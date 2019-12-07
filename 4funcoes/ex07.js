2f19e368e2452de88ce8bbdedd4d81b1957d49af
Gabriel Silva lima <gsilva-l@c2r12p3.42sp.org.br>
2019-12-07 19:02:08 -0300

    exercicio 4: funções

---
+++ 4funcoes/ex07.js
@@ -0,0 +1,41 @@
+// ************************************************************************** //
+//                                                                            //
+//                                                        :::      ::::::::   //
+//   ex07.js                                            :+:      :+:    :+:   //
+//                                                    +:+ +:+         +:+     //
+//   By: gsilva-l <marvin@42.fr>                    +#+  +:+       +#+        //
+//                                                +#+#+#+#+#+   +#+           //
+//   Created: 2019/12/07 18:54:30 by gsilva-l          #+#    #+#             //
+//   Updated: 2019/12/07 18:55:33 by gsilva-l         ###   ########.fr       //
+//                                                                            //
+// ************************************************************************** //
+
+E o que podemos fazer que não seja matemática? Podemos trabalhar com strings! 
+	E se você se recorda, as strings são um tipo de dados. Representamos as 
+cadeias de texto escrevendo dentro de aspas (“ “) literalmente.
+
+E o que podemos fazer então com as strings? Por exemplo, podemos calcular 
+quantos caracteres existem no total, e para isso iremos utilizar um recurso 
+chamado length, onde colocamos a string seguida de .length. Veja alguns exemplos:
+
+ "biblioteca".length // devolve 10
+ "babel".length     // devolve 5
+E lembre-se que também podemos somar strings! Embora que, o termo correto 
+seja concatenar, ou seja, obter uma nova string juntando duas ou mais strings.
+
+ "aa" + "bb"    // devolve "aabb"
+ "aa" + " " + "bb" // devolve "aa bb"
+Vamos testar: Iremos criar uma função chamada tamanhoNomeCompleto, 
+	que recebe um nome e um sobrenome como parâmetros, e que irá devolver 
+o tamanho total, contando um espaço extra para separar ambos:
+
+tamanhoNomeCompleto("Enzo", "Silva")
+//  devolve 10
+
+-------------------------------------------------------------------------------
+
+function tamanhoNomeCompleto(nome, sobrenome)
+{
+  var nomeTodo = nome + " " + sobrenome;
+  return nomeTodo.length
+}
---
+++ 4funcoes/ex08.js
@@ -0,0 +1,31 @@
+// ************************************************************************** //
+//                                                                            //
+//                                                        :::      ::::::::   //
+//   ex08.js                                            :+:      :+:    :+:   //
+//                                                    +:+ +:+         +:+     //
+//   By: gsilva-l <marvin@42.fr>                    +#+  +:+       +#+        //
+//                                                +#+#+#+#+#+   +#+           //
+//   Created: 2019/12/07 18:56:29 by gsilva-l          #+#    #+#             //
+//   Updated: 2019/12/07 18:57:01 by gsilva-l         ###   ########.fr       //
+//                                                                            //
+// ************************************************************************** //
+
+Para uma conferência importante, os organizadores nos pediram para escrever 
+os cartões de identificação que cada participante terá.
+
+
+
+Para isso, temos que colocar seu nome, seu sobrenome e seu título 
+(dr., dra., lic., etc) e montar uma única string.
+Escreva a função escreverCartao, que recebe como parâmetros um título, 
+um nome e um sobrenome e retorna uma única string como resultado. Por exemplo:
+
+ escreverCartao("Dra.", "Ana", "Pérez")
+"Dr. Ana Pérez"
+
+-------------------------------------------------------------------------------
+
+function escreverCartao(titulo, nome, sobre)
+{
+  return titulo + " " + nome + " " + sobre;
+}
